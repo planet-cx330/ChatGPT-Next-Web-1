8fc1d301f72a1ed07c95d97bfa1291c361787dad
planet-cx330 <110325752+planet-cx330@users.noreply.github.com>
2023-05-22 20:51:49 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>UAI.EEt</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          大学生AI助手
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
