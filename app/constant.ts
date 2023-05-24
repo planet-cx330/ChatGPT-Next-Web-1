66bd371a783b201c2493f2782b15fd7033570310
planet-cx330 <110325752+planet-cx330@users.noreply.github.com>
2023-05-24 23:49:05 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const OWNER = "lxh";
+export const REPO = "Web";
+export const REPO_URL = `https://uai.ee/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://uai.ee/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
