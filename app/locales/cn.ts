81db812ce7d5af6d8452232bcfcbdd3e6963910b
planet-cx330 <110325752+planet-cx330@users.noreply.github.com>
2023-05-24 23:55:14 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -10,7 +10,7 @@ const cn = {
     ChatItemCount: (count: number) => `${count} 条对话`,
   },
   Chat: {
-    SubTitle: (count: number) => `与 UAI.EE 的 ${count} 条对话`,
+    SubTitle: (count: number) => `与 大学生AI助手 的 ${count} 条对话`,
     Actions: {
       ChatList: "查看消息列表",
       CompressedHistory: "查看压缩后的历史 Prompt",
